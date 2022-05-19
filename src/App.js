faf8536ad1db35333dc49448f843b52694cc1e9b
김소원 <sowonkim177@gmail.com>
2022-05-19 14:21:30 +0000

    set project dir

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    	<table></table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
