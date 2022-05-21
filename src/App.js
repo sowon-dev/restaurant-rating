ac22a5eb00ad74b64c25e12aba42e90b72c45807
김소원 <sowonkim177@gmail.com>
2022-05-21 08:49:23 +0000

    design ActionAreaCard

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-		da
-		  <Card name="갈비탕" />
+		<title>맛집 별점표</title>
+		<h2>부산진구</h2>
+		  <Card area="서면" name="맛찬들왕소금구이" star="★★★★★" menu="" link="http://naver.me/xcukMcIK" />
+		  <br />
+		  <Card area="서면" name="맛찬들왕소금구이" star="★★★★★" menu="" link="http://naver.me/xcukMcIK" />
 		  </header>
     </div>
   );
--- src/component/ActionAreaCard.jsx
+++ src/component/ActionAreaCard.jsx
@@ -3,27 +3,27 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Link } from '@mui/material';
 
 export default function ActionAreaCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
+		<Link href={props.link} underline="hover" target="_blank">
+			{/*<CardMedia 
+				component="img" 
+				height="140"
+			 	image="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldbphinf.pstatic.net%2F20210204_86%2F1612443702490itpAf_JPEG%2FIgBkGhWODsI04WI8er6uWgZ6.jpg"
+			 /> */}
+			 <CardContent> 
+			  <Typography gutterBottom variant="h5" component="div">
+				{props.area} {props.name}
+			  </Typography>
+			  <Typography variant="body2" color="text.secondary">
+				{props.star}
+			  </Typography>
+        	</CardContent>
+		</Link>
       </CardActionArea>
     </Card>
   );
