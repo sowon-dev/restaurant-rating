741f2c1b23d169cc05b090eb42a7d6fa8a2b7635
김소원 <sowonkim177@gmail.com>
2022-05-24 14:55:38 +0000

    separate Data file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Card from './component/ActionAreaCard.jsx';
+import Area from './component/Area.jsx';
 
 function App() {
   return (
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
 		<title>맛집 별점표</title>
 		<h2>부산진구</h2>
-		  <Card area="서면" name="맛찬들왕소금구이" star="★★★★★" menu="" link="http://naver.me/xcukMcIK" />
-		  <br />
-		  <Card area="서면" name="덕자네방앗간(폐업)" star="★★☆☆☆" menu="" link="" />
-		  <br />
-		  <Card area="서면" name="아비꼬 NC백화점서면점" star="★★★☆☆" menu="" link="http://naver.me/G2Uxqbmj" />
+		  <Area/>
 		  </header>
     </div>
   );
