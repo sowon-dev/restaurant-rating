8f41f916989d2f41ac5cada15c7ee189586c3ccb
김소원 <sowonkim177@gmail.com>
2022-05-30 14:08:51 +0000

    add style of Header Componetn

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,18 @@
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function Header(){
 	return(
 		<header className="App-header">
-			<button href="#x" className="link">
-				맛집추가
-			</button>
+			 <Grid container spacing={2}>
+				<Grid item xs={10}>
+					<TextField id="outlined-basic" label="검색어를 입력하세요." fullWidth variant="outlined"/>
+				</Grid>
+				<Grid item xs={2}>
+					<Button href="#" variant="outlined" item xs={4} >맛집추가</Button>
+				</Grid>
+			</Grid>
 	    </header>
 	);
 }
