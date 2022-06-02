0fb1e8e210392d9475925fb55040eec742e7b0d9
김소원 <sowonkim177@gmail.com>
2022-06-02 14:44:45 +0000

    add TabPanel

--- src/component/Area.js
+++ src/component/Area.js
@@ -4,7 +4,42 @@ import dataList from "../db/data.json"
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+	
 export default function Area() {
 	const [value, setValue] = React.useState(0);
 	const handleChange = (event, newValue) => {
@@ -12,27 +47,45 @@ export default function Area() {
   	};
 	
 	return (<> 	
-	<Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-		centered
-      >
-        <Tab label="서면" />
-        <Tab label="해운대" />
-        <Tab label="센텀" />
-        <Tab label="수영" />
-		<Tab label="덕천" />
-        <Tab label="사상" />
-        <Tab label="부산진" />
-      </Tabs>
-    </Box>	
-		
-	{dataList.restaurants.map((data, index) => (
-		<Card key={index} area={data.areaNm} name={data.name} star={data.star} menu={data.menu} link={data.link} />
-	) )}
+	<Box sx={{ width: '100%' }}>
+		<Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+		  <Tabs
+			value={value}
+			onChange={handleChange}
+			variant="scrollable"
+			scrollButtons="auto"
+			aria-label="scrollable auto tabs example"
+			centered
+		  >
+			<Tab label="서면" />
+			<Tab label="해운대" />
+			<Tab label="센텀" />
+			<Tab label="수영" />
+			<Tab label="덕천" />
+			<Tab label="사상" />
+			<Tab label="부산진" />
+		  </Tabs>
+    	</Box>
+		<TabPanel value={value} index={0}>
+			{dataList.restaurants.filter(data => ( data.areaNm === '서면')).map((data, index) => (
+				<Card key={index} area={data.areaNm} name={data.name} star={data.star} menu={data.menu} link={data.link} />
+			))}
+		</TabPanel>
+		<TabPanel value={value} index={1}>
+			{dataList.restaurants.filter(data => ( data.areaNm === '해운대')).map((data, index) => (
+				<Card key={index} area={data.areaNm} name={data.name} star={data.star} menu={data.menu} link={data.link} />
+			))}
+		</TabPanel>
+		<TabPanel value={value} index={2}>
+			{dataList.restaurants.filter(data => ( data.areaNm === '센텀')).map((data, index) => (
+				<Card key={index} area={data.areaNm} name={data.name} star={data.star} menu={data.menu} link={data.link} />
+			))}
+		</TabPanel>
+	</Box>
+
 	</>);
 }
+
+// {dataList.restaurants.map((data, index) => (
+//<Card key={index} area={data.areaNm} name={data.name} star={data.star} menu={data.menu} link={data.link} />
+// ))} 
