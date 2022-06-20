be2d13867909aa573791c3adf01ebf2a28bb4c2b
김소원 <sowonkim177@gmail.com>
2022-06-20 14:08:06 +0000

    fix Header css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import './App.css';
 import Header from './component/Header';
-import Area from './component/Area.js';
+import Area from './component/Area';
+import Write from './component/Write';
+import { Route, Link, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-	  <title>맛집 별점표</title>
-	  <Area/>
-    </div>
+	<div className="App">	  
+	  <div>
+		  <Header />
+	  	  <title>맛집 별점표</title>
+	  </div>
+	  <div>
+		  <Routes>
+			<Route path="/" component={Area} exact={true} />
+			<Route path="/write" component={Write} />
+		  </Routes>
+      </div>
+	</div>
   );
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Write from '/component/Write.js';
-import { Route } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
 
 class Header extends Component {
 	render() {
+		const Item = styled(Paper)(({ theme }) => ({
+		  //backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+		  backgroundColor: '#142d4c',
+		  ...theme.typography.body2,
+		  padding: theme.spacing(1),
+		  textAlign: 'center',
+		  color: theme.palette.text.secondary,
+		}));
+		
 		return(
 			<header className="App-header">
-				 <Grid container >
-					<Grid item xs={10}>
-						<TextField id="outlined-basic" label="검색어를 입력하세요." fullWidth variant="outlined" item />
-						<Button href="#" variant="outlined" item >검색</Button>
+				 <Grid container columns={12}>
+					<Grid item="ture" xs="auto"> 
+						<Item><TextField id="outlined-basic" label="검색어를 입력하세요." fullWidth variant="filled" focused item="ture" /></Item>
 					</Grid>
-					<Grid item xs={2}>
-						<Button href="/Write" variant="outlined" item >맛집추가</Button>
-						<Route path="/Write" component={Write} >링크</Route>
+					<Grid item="ture" xs={2}>
+						<Item><Button href="#" variant="contained" item="ture" >검색</Button></Item>
+					</Grid>
+					<Grid item="ture" xs={2}>
+						<Item><Button href="/write" variant="contained" item="ture" >맛집추가</Button></Item>
 					</Grid>
 				</Grid>
 			</header>
